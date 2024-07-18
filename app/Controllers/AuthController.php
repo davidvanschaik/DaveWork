e7372e57be3191618bb0d7ac2f87b7de74413878
David van Schaik <david.vanschaik@myparcel.nl>
2024-07-18 23:25:23 +0200

    Working on build in login

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -18,11 +18,12 @@ class AuthController
     public function make(Request $request): void
     {
 //        $errors = errors();
-        $errors = [
-            'test' => 'dit is een test bericht',
-            'test2' => 'dit is ook een test bericht'
-        ];
-        echo view('login', ['errors' => $errors]);
+//        $errors = [
+//            'test' => 'dit is een test bericht',
+//            'test2' => 'dit is ook een test bericht'
+//        ];
+//        echo view('login', ['errors' => $errors]);
+        echo base64_encode('e86bb2e7d8d517adacbd9da423ae4fd5001db888');
     }
 
     public function login(Request $request): void
