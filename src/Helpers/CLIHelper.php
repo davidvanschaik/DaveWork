f37f4c41f4db02ea51107cf83fc7c390616953e9
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-05 14:19:43 +0100

    added echo function

--- src/Helpers/CLIHelper.php
+++ src/Helpers/CLIHelper.php
@@ -13,4 +13,9 @@ class CLIHelper
     public const MAGENTA = "\033[0;35m";
     public const CYAN = "\033[0;36m";
     public const WHITE = "\033[0;37m";
+
+    public static function echo(string $color, string $message): string
+    {
+        return self::{$color} . $message . self::RESET . PHP_EOL;
+    }
 }
