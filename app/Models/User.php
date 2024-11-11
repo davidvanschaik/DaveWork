b927d544445bf58e0b83c4ae8e3580bda534f8ed
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-11 18:50:02 +0100

    Delete comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-//use Src\Database\Eloquent\Model;
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
