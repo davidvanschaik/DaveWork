9aa3fe9aa78e0fc3545ce897ccc440e649724149
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-15 10:29:22 +0100

    split Host into Server, and Request

--- src/Console/Commands/Server/Host.php
+++ src/Console/Commands/Server/Host.php
@@ -5,86 +5,20 @@ namespace Src\Console\Commands\Server;
 use Src\Console\Response as CLI;
 use Src\Console\Commands\Command;
 
-class Host implements Command
+class Host extends Server implements Command
 {
-    private int $port;
-
     public function __construct()
     {
-        $this->port = 8000;
-        chdir('public');
-        $this->terminateServer($this->fileName());
+        parent::__construct();
+        $this->clear($this->fileName());
     }
 
     public function __invoke(): void
     {
-        while ($this->availablePort($server = popen("php -S 127.0.0.1:{$this->port} 2>&1", 'r'))) {
+        while ($this->availablePort($server = popen("php -S {$this->server()} 2>&1", 'r'))) {
             sleep(1);
             $this->port++;
         }
         $this->startServer($server);
-    }
-
-    private function availablePort(mixed $process): bool
-    {
-        if (str_contains(fgets($process), 'Address already in use')) {
-            echo '    ' . CLI::echo('GRAY', "Port: [127.0.0.1:{$this->port}] already in use... \n");
-            pclose($process);
-            return true;
-        }
-        return false;
-    }
-
-    private function startServer(string $server): void
-    {
-        sleep(1);
-        echo CLI::block() . CLI::echo('GREEN', " Server running on [http://127.0.0.1:{$this->port}] \n \n");
-        echo CLI::echo('YELLOW', "    Press Ctrl+C to stop the server \n \n");
-
-        $this->validateResponse($server);
-    }
-
-    private function validateResponse($server): void
-    {
-        $key = 0;
-
-        while (! feof($server)) {
-            $this->handleData($data = $this->getData(), $key);
-            $key =+ count($data);
-            pcntl_signal_dispatch();
-        }
-    }
-
-    private function getData(): array
-    {
-        return file($this->fileName(), 2 | 4);
-    }
-
-    private function fileName(): string
-    {
-        return __DIR__ . '/../../../../storage/logs/server.log';
-    }
-
-    private function handleData(array $data, int $key): void
-    {
-        if (count($data) !== 0 || $key < count($data)) {
-            for ($x = $key; $x < count($data); $x++) {
-                $this->displayActivity(explode(' ', $data[$x]));
-            }
-        }
-    }
-
-    private function displayActivity(array $info): void
-    {
-        echo '    ' . CLI::echo('GRAY', $info[0]) . "{$info[1]} {$info[2]} {$info[3]} ";
-        echo CLI::GRAY . str_repeat('.', 160 - array_sum(array_map('strlen', $info))) . " ~ 0s \n";
-    }
-
-    private function terminateServer($file): bool
-    {
-        return pcntl_signal(SIGINT, function () use ($file) {
-            file_put_contents($file, '');
-            exit(0);
-        });
     }
 }
--- src/Console/Commands/Server/router.php
+++ src/Console/Commands/Server/router.php
@@ -1,10 +1,11 @@
 <?php
 
 $log = sprintf(
-    "%s %s %s %s\n",
+    "%s %s %s %s %s\n",
     date('Y-m-d'),
     date('H:i:s'),
     $_SERVER['REQUEST_METHOD'],
     $_SERVER['REQUEST_URI'],
+    number_format((microtime(true) - $_SERVER['REQUEST_TIME']) * 1000, 2)
 );
 file_put_contents(__DIR__ . '/../../../../storage/logs/server.log', $log, FILE_APPEND);
