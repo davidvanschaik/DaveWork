5c81405e3e38e28e2801cf7967f7f1b79e9dc96b
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-19 20:58:50 +0100

    Construct was not necessary

--- database/Factories/UserFactory.php
+++ database/Factories/UserFactory.php
@@ -3,26 +3,12 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Collection;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    public function __construct(
-        $count = null,
-        ?Collection $states = null,
-        ?Collection $has = null, ?Collection $for = null,
-        ?Collection $afterMaking = null,
-        ?Collection $afterCreating = null,
-        $connection = null,
-        ?Collection $recycle = null
-    ) {
-        parent::__construct($count, $states, $has, $for, $afterMaking, $afterCreating, $connection, $recycle);
-        $this->faker = \Faker\Factory::create();
-    }
-
     /**
      * The current password being used by the factory.
      */
@@ -35,11 +21,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = \Faker\Factory::create();
+
         return [
-            'username' => $this->faker->userName,
-            'email' => $this->faker->unique()->safeEmail,
-            'password' => $this->faker->password,
-            'phone' => $this->faker->phoneNumber,
+            'username' => $faker->userName,
+            'email' => $faker->unique()->safeEmail,
+            'password' => $faker->password,
+            'phone' => $faker->phoneNumber,
         ];
     }
 }
