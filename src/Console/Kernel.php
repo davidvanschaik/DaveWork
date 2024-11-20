07f48d51f47b0adc7676bf0ad6f4e8fbe0035715
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-21 00:00:31 +0100

    Updated COmmands

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -16,18 +16,16 @@ class Kernel
 
     private function callFunction(array $arg): void
     {
-        $class = __NAMESPACE__ . $this->getDirectory($arg);
+        $class = $this->getDirectory($arg);
         (new $class($arg))();
     }
 
     private function getDirectory(array $arg): string
     {
-         return match ($arg[0]) {
-            'db' => "\\Commands\\Database\\$arg[1]",
-            'make' => "\\Commands\\Make\\$arg[0]",
-            'host' => "\\Commands\\Server\\$arg[0]",
-             'help' => exit,
-            default => null
+         return __NAMESPACE__ . "\\Commands\\" . match ($arg[0]) {
+            'db' => "$arg[1]Command",
+            'make', 'host' => "$arg[0]Command",
+            'help' => exit
         };
     }
 }
