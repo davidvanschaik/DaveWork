98c82f717538237a2603409678210b20dfa77b7e
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-20 22:52:59 +0100

    Database helper

---
+++ src/Helpers/DatabaseHelper.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Src\Helpers;
+
+use Src\Console\Response as CLI;
+use Src\Providers\DatabaseServiceProvider as DB;
+
+class DatabaseHelper
+{
+    private static bool $bool;
+
+    public static function getMigrations(bool $bool): array
+    {
+        self::$bool = $bool;
+        return self::sortMigrations(array_map(function ($migration) {
+            return pathInfo($migration, PATHINFO_FILENAME);
+        }, glob('database/Migrations/*')), $bool);
+    }
+
+    private static function sortMigrations(array $migrations, bool $bool): array
+    {
+        return !$bool ? $migrations : array_reverse($migrations);
+    }
+
+    public static function migrationsFile(array $migrations, array $tables): array
+    {
+        return array_map(function ($key) use ($migrations) {
+            return $migrations[$key];
+        }, array_keys($tables));
+    }
+
+    public static function tableExist(array $migrations): array
+    {
+        return array_filter($migrations, function ($migration) {
+            if (DB::tableExists(self::getTable($migration)) == self::$bool) {
+                return $migration;
+            }
+        });
+    }
+
+    private static function getTable(string $migration): string
+    {
+        $table = explode('_', $migration);
+        return $table[2] . 's';
+    }
+
+    public static function validateTables(array $tables, string $command): void
+    {
+        if (empty($tables)) {
+            echo CLI::block() . " Nothing to $command. \n \n";
+            exit;
+        }
+    }
+}
