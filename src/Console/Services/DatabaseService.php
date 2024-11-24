d65da53e35061c186e51196d959610a5235651e8
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-24 21:48:10 +0100

    edited so  src/Database migrations will be executed and also executed Migrations will be inserted

--- src/Console/Services/DatabaseService.php
+++ src/Console/Services/DatabaseService.php
@@ -5,12 +5,53 @@ namespace Src\Console\Services;
 use Src\Console\Response as CLI;
 use Src\Core\App;
 use Src\Database\Repositories\MigrationRepository;
-use Src\Helpers\DatabaseHelper as Helper;
 use Src\Providers\DatabaseServiceProvider as DB;
 
 class DatabaseService
 {
-    public function __construct()
+    private array $migrations;
+
+    public function validateMigrations(string $command, array $migrations): object
+    {
+        $this->migrations = $migrations;
+        array_map(function ($migrations) use ($command) {
+            $this->noPendingMigrations($migrations, $command);
+        }, $migrations);
+
+        return $this;
+    }
+
+    private function noPendingMigrations(array $migrations, string $command): void
+    {
+        if (empty($migrations)) {
+            echo CLI::block() . " Nothing to $command. \n \n";
+            exit;
+        }
+    }
+
+    public function startMigrating(string $message, string $func): void
+    {
+        echo CLI::block() . " $message migrations. \n \n";
+        $this->runMigrations($func);
+    }
+
+    public function runMigrations(string $func): void
+    {
+        array_map(function ($migration, $key) use ($func) {
+            $this->requireFile($key, $func);
+        }, $this->migrations, array_keys($this->migrations));
+    }
+
+    private function requireFile(string $key, string $func): void
+    {
+        $this->setInstance();
+        foreach ($this->migrations[$key] as $file) {
+            require_once "$key/Migrations/$file.php";
+        }
+        $this->executeMigration($key, $func);
+    }
+
+    private function setInstance(): void
     {
         App::getInstance()->singleton('MigrationRegistration', function () {
             return new class {
@@ -19,43 +60,35 @@ class DatabaseService
         });
     }
 
-    public function startMigrations(array $migrations, array $tables, string $message): object
+    private function executeMigration($key, $func): void
     {
-        echo CLI::block() . " $message migrations. \n \n";
-        return $this->requireFile(Helper::migrationsFile($migrations, $tables));
-    }
-
-    public function requireFile(array $migrations, string $path = 'database'): object
-    {
-        foreach ($migrations as $file) {
-            require_once "$path/Migrations/$file.php";
-        }
-        return $this;
-    }
-
-    public function runMigrationWithoutOutput(array $migrations, string $func)
-    {
-//        var_dump(App::getInstance()->resolve('MigrationRegistration')->classes);
-        array_map(function ($file, $class) use ($func) {
+        $repo = new MigrationRepository();
+        array_map(function ($file, $class) use ($func, $repo) {
             $time = $this->callFunction($class, $func);
-//            MigrationRepository::insert($file);
-        }, $migrations, App::getInstance()->resolve('MigrationRegistration')->classes);
+            $this->validateOutput($class, $file, $time, $func, $repo);
+        }, $this->migrations[$key], App::getInstance()->resolve('MigrationRegistration')->classes);
     }
 
-    public function runMigrationWithOutput(array $migrations, string $func): void
-    {
-        array_map(function ($file, $class) use ($func) {
-            $time = $this->callFunction($class, $func, DB::get());
-            $this->showOutput($time, $file);
-        }, $migrations, App::getInstance()->resolve('MigrationRegistration')->classes);
-    }
-
-    private function callFunction(object $class, string $function): string
+    private function callFunction(mixed $class, string $function): string
     {
         $startTime = microtime('true');
         $class->$function(DB::get());
         $endTime = microtime(true);
         return number_format(($endTime - $startTime) * 1000, 2);
+    }
+
+    private function validateOutput(
+        object $class,
+        string $fileName,
+        string $time,
+        string $func,
+        MigrationRepository $repo
+    ): void
+    {
+        if (! $class->silent) {
+            $this->showOutput($time, $fileName);
+            $repo->$func($fileName);
+        }
     }
 
     private function showOutput(string $time, string $migration): void
--- src/Helpers/DatabaseHelper.php
+++ src/Helpers/DatabaseHelper.php
@@ -2,53 +2,70 @@
 
 namespace Src\Helpers;
 
-use Src\Console\Response as CLI;
+use Src\Database\Repositories\MigrationRepository;
 use Src\Providers\DatabaseServiceProvider as DB;
 
 class DatabaseHelper
 {
     private static bool $bool;
 
-    public static function getMigrations(int $bool, string $path = 'database'): array
+    public static function validateMigrations(bool $bool): array
     {
         self::$bool = $bool;
+        return self::pendingMigrations(self::getExecutedMigrations(), self::collectMigrations());
+    }
+
+    private static function pendingMigrations(array $executed, array $all): array
+    {
+        $migrations = array_map(function ($dir) use ($all, $executed) {
+            return array_filter($all[$dir], function ($migration) use ($executed) {
+                return in_array($migration, $executed) == self::$bool;
+            });
+        }, array_keys($all));
+
+        return array_combine(array_keys($all), $migrations);
+    }
+
+    private static function getExecutedMigrations(): array
+    {
+        if (self::tableExists('migrations')) {
+            return array_map(function ($executed) {
+                return $executed->migration;
+            }, (new MigrationRepository())->getAll());
+        }
+        return [];
+    }
+
+    public static function tableExists(string $table): bool
+    {
+        return DB::get()->hasTable($table);
+    }
+
+    private static function collectMigrations(): array
+    {
+        return array_reduce(self::getDirectories(), function ($migrations, $dir) {
+            $migrations[$dir] = self::FetchMigrations($dir);
+            return $migrations;
+        }, []);
+    }
+
+    private static function getDirectories(): array
+    {
+        if (self::tableExists('migrations') == self::$bool && !self::$bool) {
+            return ['src/Database', 'database'];
+        }
+        return ['database'];
+    }
+
+    private static function FetchMigrations(string $dir): array
+    {
         return self::sortMigrations(array_map(function ($migration) {
             return pathInfo($migration, PATHINFO_FILENAME);
-        }, glob("$path/Migrations/*")), $bool);
+        }, glob("$dir/Migrations/*")));
     }
 
-    private static function sortMigrations(array $migrations, bool $bool): array
+    private static function sortMigrations(array $migrations): array
     {
-        return !$bool ? $migrations : array_reverse($migrations);
-    }
-
-    public static function migrationsFile(array $migrations, array $tables): array
-    {
-        return array_map(function ($key) use ($migrations) {
-            return $migrations[$key];
-        }, array_keys($tables));
-    }
-
-    public static function tableExist(array $migrations): array
-    {
-        return array_filter($migrations, function ($migration) {
-            if (DB::tableExists(self::setTableName($migration)) == self::$bool) {
-                return $migration;
-            }
-        });
-    }
-
-    public static function setTableName(string $migration): string
-    {
-        $table = explode('_', $migration);
-        return $table[2] . 's';
-    }
-
-    public static function validateTables(array $tables, string $command): void
-    {
-        if (empty($tables)) {
-            echo CLI::block() . " Nothing to $command. \n \n";
-            exit;
-        }
+        return !self::$bool ? $migrations : array_reverse($migrations);
     }
 }
