21914b5af1bae45e79a459f60dcdb071d9a594d9
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-27 16:10:39 +0100

    Connection retrieved from serviceProvider

--- src/Database/Connections/Database.php
+++ src/Database/Connections/Database.php
@@ -2,19 +2,19 @@
 
 declare(strict_types=1);
 
-namespace Src\Providers;
+namespace Src\Database\Connections;
 
 use Illuminate\Database\Query\Builder as Schema;
 use Illuminate\Database\Schema\Builder as Query;
-use Src\Database\Connections\Connection;
+use Src\Core\App;
 
-class DatabaseServiceProvider
+class Database
 {
     protected static Query $schema;
 
-    public static function register(array $options): void
+    public function register(): void
     {
-        self::$schema = (new Connection($options))->getSchema();
+        self::$schema = App::getInstance()->resolve('connection');
     }
 
     public static function get(): Query
