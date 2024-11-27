a27dc980a268ff9cec2ad19bd6d5d4c5b3c96d94
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-27 15:01:39 +0100

    Added ServiceProvider for each class

---
+++ src/Providers/ConnectionServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Src\Providers;
+
+use Src\Core\ServiceProvider;
+use Src\Database\Connections\Connection;
+use Src\Database\Connections\Database;
+
+class ConnectionServiceProvider extends ServiceProvider
+{
+    function register(): void
+    {
+        $options = require_once __DIR__ . '/../../config/database.php';
+        $this->app->singleton('connection', function () use ($options) {
+            return (new Connection($options))->getSchema();
+        });
+
+        (new Database())->register();
+    }
+}
