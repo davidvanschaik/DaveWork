3a651ff65f0a806191cc33ce666710c38c14c8ff
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-28 17:21:39 +0100

    Made app.php in config for storing all the service providers

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    \Src\Providers\RequestServiceProvider::class,
+    \Src\Providers\HandlersServiceProvider::class,
+    \Src\Providers\SessionServiceProvider::class,
+    \Src\Providers\ViewServiceProvider::class,
+    \Src\Providers\ConnectionServiceProvider::class,
+];
