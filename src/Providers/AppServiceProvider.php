6238a11411f3676b49d6620a63cdf429b71dc640
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-28 17:20:39 +0100

    New AppServiceProvider class

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -5,35 +5,12 @@ declare(strict_types=1);
 namespace Src\Providers;
 
 use Src\Core\ServiceProvider;
-use Src\Exceptions\SyntaxExceptions;
-use Src\Handlers\ErrorHandler;
-use Src\Http\Request;
-use Src\Http\Session;
-use Src\View\View;
 
-// TODO breaking up the service providers in categories
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->prototype('request', function () {
-            return new Request();
-        });
-
-        $this->app->singleton('session', function () {
-            return new Session();
-        });
-
-        $this->app->singleton('view', function () {
-            return new View();
-        });
-
-        $this->app->prototype('error', function () {
-            return new ErrorHandler();
-        });
-
-        $this->app->prototype('PHP.errors', function () {
-            return new SyntaxExceptions();
-        });
+        $providers = require __DIR__ . "/../../config/app.php";
+        array_map(fn ($provider) => (new $provider($this->app))->register(), $providers);
     }
 }
