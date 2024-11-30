45eab338a23a7d94607d9a474a5a6f8d4c50ff7c
David van Schaik <davidvan.schaik00@gmail.com>
2024-11-30 19:21:39 +0100

    env variables in .env are working

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'driver'    => 'mysql',
-    'host'      => '127.0.0.1',
-    'port'      => '3306',
-    'database'  => 'Binsta',
-    'username'  => 'root',
-    'password'  => '',
+    'driver'    => $_ENV['DB_CONNECTION'],
+    'host'      => $_ENV['DB_HOST'],
+    'port'      => $_ENV['DB_PORT'],
+    'database'  => $_ENV['DB_NAME'],
+    'username'  => $_ENV['DB_USERNAME'],
+    'password'  => $_ENV['DB_PASSWORD'],
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
