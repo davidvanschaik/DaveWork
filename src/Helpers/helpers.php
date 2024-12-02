caa423b3e79cc981b86b4e5ffe4bc0a5369bf024
David van Schaik <davidvan.schaik00@gmail.com>
2024-12-02 07:37:50 +0100

    Checked if every function already exist

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,53 +1,51 @@
 <?php
 
 declare(strict_types=1);
-
 require __DIR__ . '/../../vendor/autoload.php';
 
 use Src\Core\App;
 use Src\Routing\RouteRegistration as Route;
-use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
-$dotenv->load();
-
-function dd(mixed $var): void
-{
-    $location = debug_backtrace();
-    echo "<pre>";
-
-    echo 'File: ' . $location[0]['file'] . PHP_EOL;
-    echo 'Line: ' . $location[0]['line'] . PHP_EOL . PHP_EOL;
-    var_dump($var);
-
-    echo "</pre>";
-    die();
-}
-
-function view(string $templateName, array $data = []): string
-{
-    $view = App::getInstance()->resolve('view');
-    return $view->render($templateName, $data);
-}
-
-function redirect(string $routeName): void
-{
-    $request = App::getInstance()->resolve('request');
-    if ($routeName == 'back') {
-        header("Location:" . $request->uri());
-    }
-
-    foreach (Route::$routes as $route) {
-       foreach ($route as $info) {
-           if ($info->name == $routeName) {
-               header("Location:{$info->uri}");
-           }
-       }
+if (!function_exists('dd')) {
+    function dd(mixed $var): void
+    {
+        $location = debug_backtrace();
+        echo "<pre>";
+        echo 'File: ' . $location[0]['file'] . PHP_EOL;
+        echo 'Line: ' . $location[0]['line'] . PHP_EOL . PHP_EOL;
+        var_dump($var);
+        echo "</pre>";
+        die();
     }
 }
 
-function errors()
-{
-    $session = App::getInstance()->resolve('session');
-    return $session->getErrors('errors');
+if (!function_exists('view')) {
+    function view(string $templateName, array $data = []): string
+    {
+        $view = App::getInstance()->resolve('view');
+        return $view->render($templateName, $data);
+    }
+}
+
+if (!function_exists('redirect')) {
+    function redirect(string $routeName): void
+    {
+        $request = App::getInstance()->resolve('request');
+        if ($routeName == 'back') {
+            header("Location:" . $request->uri());
+        }
+        array_map(fn($route) => array_map(function ($info) use ($routeName) {
+            if ($info->name == $routeName) {
+                header("Location:{$info->uri}");
+            }
+        }, $route), Route::$routes);
+    }
+}
+
+if (! function_exists('errors')) {
+    function errors()
+    {
+        $session = App::getInstance()->resolve('session');
+        return $session->getErrors('errors');
+    }
 }
